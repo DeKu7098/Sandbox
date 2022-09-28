f11a3557eedb3ee446dfb637de8ab6913d996b19
DeKu7098 <91478335+DeKu7098@users.noreply.github.com>
2022-09-29 00:05:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Memo from "./Memo";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Memo />
+    </div>
+  );
+}
---
+++ src/Memo.js
@@ -0,0 +1,30 @@
+import { useMemo, useState } from "react";
+
+function Memo() {
+  const [counterOne, setCounterOne] = useState(0);
+  const [counterTwo, setCounterTwo] = useState(0);
+
+  const incrementOne = () => {
+    setCounterOne(counterOne + 1);
+  };
+  const incrementTwo = () => {
+    setCounterTwo(counterTwo + 2);
+  };
+  const isEven = useMemo(() => {
+    console.warn("....");
+    let i = 0;
+    while (i < 2000) i++;
+
+    return counterOne % 2 === 0;
+  }, [counterOne]);
+
+  return (
+    <div>
+      <button onClick={incrementOne}>Counter One - {counterOne}</button>
+      <span>{isEven ? "Even" : "Odd"}</span>
+      <button onClick={incrementTwo}>Counter Two - {counterTwo}</button>
+    </div>
+  );
+}
+
+export default Memo;
